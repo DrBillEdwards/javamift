9739cd00d6c75662df135df452f55b06c09d9857
DrBillEdwards <DrBillEdwards@yahoo.com>
2016-01-28 00:41:17 -0500

    added sequential downtimes

--- SimpleMift.java
+++ SimpleMift.java
@@ -14,8 +14,8 @@ public class SimpleMift
     static boolean appendOutput = true;
     static boolean interjectDownTime = false; // EITHER
     static boolean interjectDownTimes = true; // OR
-    static String REPORT_FILE_NAME = "report1.1.txt";
-    static String OUTPUT_FILE_NAME = "output1.1.txt";
+    static String REPORT_FILE_NAME = "report1.txt";
+    static String OUTPUT_FILE_NAME = "output1.txt";
     static int NUM_RUNS = 101;
     static String NEW_LINES = "\n"; // Toggle newline types
     // static String NEW_LINES = "\r\n";
@@ -44,6 +44,9 @@ public class SimpleMift
     static boolean sta1failed = false;
     static boolean sta2failed = false;
     static boolean sta3failed = false;
+    static boolean sta4failed = false;
+    static boolean sta5failed = false;
+    static boolean sta6failed = false;
     static int staNumFailed = 0;
     static Timer timer1, timer2, timer3;
 
@@ -220,25 +223,15 @@ public class SimpleMift
                             runHour++;
                             t1 += SECS_PER_HOUR / 2;
                         }
-                        else if((runHour == 10) && (t >= (SECS_PER_HOUR * 10)))
-                        {
-                            displayText = "Deliberate 1/2 hour down time, hour " + runHour;
-                            System.out.println(displayText);
-                            outputs[i++] = displayText;
-                            try {Thread.sleep(SECS_PER_HOUR / 2);} catch (Exception exception) {}
-                            runHour++;
-                            t1 += SECS_PER_HOUR / 2;
-                        }
                     }
                 }
                 Random rand = new Random();
                 r1 = new Random().nextDouble();
                 r2 = new Random().nextDouble();
                 r3 = new Random().nextDouble();
-                sta1failed = ((assemblyLine1[0] == 1) && (r1 >= .98));
-                sta2failed = ((assemblyLine1[1] == 1) && (r2 >= .95));
-                sta3failed = ((assemblyLine1[2] == 1) && (r3 >= .99));
-                // if((((assemblyLine1[0] + assemblyLine1[1] + assemblyLine1[2]) > 0) && !(sta1failed || sta2failed || sta3failed)) || ())
+                sta1failed = ((assemblyLine1[0] == 1) && (r1 > .98));
+                sta2failed = ((assemblyLine1[1] == 1) && (r2 > .95));
+                sta3failed = ((assemblyLine1[2] == 1) && (r3 > .99));
                 if (!(sta1failed || sta2failed || sta3failed))
                 {
                     if (buffer1 < 2)
@@ -262,9 +255,24 @@ public class SimpleMift
                     else if(sta3failed){staNumFailed = 3;}
                     System.out.println("FAILED LINE 1, STATION: " + staNumFailed);
                     outputs[i++] = "FAILED LINE 1, STATION: " + staNumFailed;
-                    try {Thread.sleep(DOWN_TIME);}
-                    catch (Exception exception) {}
-                    t1 += DOWN_TIME;
+                    if(sta1failed)
+                    {
+                        try {Thread.sleep(DOWN_TIME);}
+                        catch (Exception exception) {}
+                        t1 += DOWN_TIME;
+                        if(sta2failed)
+                        {
+                            try {Thread.sleep(DOWN_TIME);}
+                            catch (Exception exception) {}
+                            t1 += DOWN_TIME;
+                            if(sta3failed)
+                            {
+                                try {Thread.sleep(DOWN_TIME);}
+                                catch (Exception exception) {}
+                                t1 += DOWN_TIME;
+                            }
+                        }
+                    }
                     displayText = "t1: " + Math.round(t1) + " ";
                     displayText += "assembly line 1: " + assemblyLine1[0] + assemblyLine1[1] + assemblyLine1[2] + " ";
                     displayText += "buffer1: " + buffer1;
@@ -283,10 +291,10 @@ public class SimpleMift
                 r4 = new Random().nextDouble();
                 r5 = new Random().nextDouble();
                 r6 = new Random().nextDouble();
-                sta1failed = ((assemblyLine2[0] == 1) && (r4 >= .92));
-                sta2failed = ((assemblyLine2[1] == 1) && (r5 >= .90));
-                sta3failed = ((assemblyLine2[2] == 1) && (r6 >= .94));
-                if(!(sta1failed || sta2failed || sta3failed))
+                sta4failed = ((assemblyLine2[0] == 1) && (r4 > .92));
+                sta5failed = ((assemblyLine2[1] == 1) && (r5 > .90));
+                sta6failed = ((assemblyLine2[2] == 1) && (r6 > .94));
+                if(!(sta4failed || sta5failed || sta6failed))
                 {
                     output += assemblyLine2[2];
                     assemblyLine2[2] = assemblyLine2[1];
@@ -307,14 +315,29 @@ public class SimpleMift
                 }
                 else
                 {
-                    if(sta1failed){staNumFailed = 4;}
-                    else if(sta2failed){staNumFailed = 5;}
-                    else if(sta3failed){staNumFailed = 6;}
+                    if(sta4failed){staNumFailed = 4;}
+                    else if(sta5failed){staNumFailed = 5;}
+                    else if(sta6failed){staNumFailed = 6;}
                     System.out.println("FAILED LINE 2, STATION: " + staNumFailed);
                     outputs[i++] = "FAILED LINE 2, STATION: " + staNumFailed;
-                    try {Thread.sleep(DOWN_TIME);}
-                    catch(Exception exception) {}
-                    t2 += DOWN_TIME;
+                    if(sta4failed)
+                    {
+                        try {Thread.sleep(DOWN_TIME);}
+                        catch (Exception exception) {}
+                        t2 += DOWN_TIME;
+                        if(sta5failed)
+                        {
+                            try {Thread.sleep(DOWN_TIME);}
+                            catch (Exception exception) {}
+                            t2 += DOWN_TIME;
+                            if(sta6failed)
+                            {
+                                try {Thread.sleep(DOWN_TIME);}
+                                catch (Exception exception) {}
+                                t2 += DOWN_TIME;
+                            }
+                        }
+                    }
                     displayText = "t2: " + t2 + " ";
                     displayText += "assembly line 2: " + assemblyLine2[0] + assemblyLine2[1] + assemblyLine2[2] + " ";
                     displayText += "buffer1: " + buffer1;
